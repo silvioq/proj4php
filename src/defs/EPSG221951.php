0ba1cd554402aa28e61d32dadee54f560b859d2e
Silvio <silvioq@gmail.com>
2016-03-19 07:16:18 -0300

    GAUSS KRUGGER BUENOS AIRES projection

---
+++ src/defs/EPSG221951.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    "EPSG:221951" => "+title=Gauss Krugger Buenos Aires +proj=tmerc +lat_0=-34.6297166 +lon_0=-58.4627 +k=0.999998 +x_0=100000 +y_0=100000 +ellps=intl +towgs84=-148,136,90,0,0,0,0 +units=m +no_defs",
+];
